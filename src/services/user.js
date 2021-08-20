c65af65dc0694d186c06e869b12c47e329a86b03
Tássio <tassio72@hotmail.com>
2021-08-20 20:48:03 -0300

    chore: remove changeMethod

--- src/services/user.js
+++ src/services/user.js
@@ -1,4 +1,4 @@
-import { STORAGE_KEYS, EVENT_KEYS, emitter } from "../dealful";
+import { STORAGE_KEYS } from "../dealful";
 
 export const fetchUser = () => {
   return new Promise((resolve) => {
@@ -20,25 +20,4 @@ export const fetchUser = () => {
 export const firstLoadUserData = async () => {
   console.log("%c first load user data - auth", "color: red;");
   return await fetchUser();
-};
-
-export const changeUser = async () => {
-  try {
-    const fakeUserData = {
-      name: { first: "Seu", last: "Madruga" },
-      age: 22,
-      email: "some@some",
-      picture: {
-        large:
-          "https://imagens3.ne10.uol.com.br/blogsne10/social1/uploads/2021/05/Ramon-Valdes-como-Seu-Madruga-em-Chaves.jpg",
-      },
-    };
-    localStorage.setItem(STORAGE_KEYS.USER_DATA, JSON.stringify(fakeUserData));
-    console.log("%c set user called - store", "color: red;");
-    return await emitter.emit(EVENT_KEYS.CHANGE_USER, () => {
-      return fakeUserData;
-    });
-  } catch (error) {
-    console.error("user set erro request", error);
-  }
 };
